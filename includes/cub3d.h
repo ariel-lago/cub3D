3ab1366217d978e4ac5f7edf6fbc5bb5869687e1
Ariel Lago Garcia <alago-ga@c4c3c2.42berlin.de>
2026-02-23 16:42:16 +0100

    includes

---
+++ includes/cub3d.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alago-ga <alago-ga@student.42berlin.d>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 15:44:44 by alago-ga          #+#    #+#             */
+/*   Updated: 2026/02/23 15:50:27 by alago-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <math.h>
+
+# include "mlx.h"
+# include "libft.h"
+# include "defines.h"
+# include "parse.h"
+
+#endif
