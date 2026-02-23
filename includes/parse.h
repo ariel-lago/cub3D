937f3fe89ea7954a5d95c09b72be4f3cd65bdb57
Ariel Lago Garcia <alago-ga@c4c3c2.42berlin.de>
2026-02-23 18:04:02 +0100

    first steps

--- includes/parse.h
+++ includes/parse.h
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   defines.h                                          :+:      :+:    :+:   */
+/*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alago-ga <alago-ga@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/23 17:04:29 by alago-ga          #+#    #+#             */
-/*   Updated: 2026/02/23 17:08:04 by alago-ga         ###   ########.fr       */
+/*   Created: 2026/02/23 17:32:23 by alago-ga          #+#    #+#             */
+/*   Updated: 2026/02/23 17:34:14 by alago-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DEFINES_H
-# define DEFINES_H
+#ifndef PARSE_H
+# define PARSE_H
 
-# include "cub3D.h"
+# include "cub3d.h"
 
-typedef struct s_map
-{
-	int		map_width;
-	int		map_height;
-	int		player_x;
-	int		player_y;
-	int		player_dir;
-	char	*walls[4];
-	char	*floor_color;
-	char	*ceiling_color;
-}	t_map;
+int	parse(t_map	*map, char *filename);
 
 #endif
--- src/parse.c
+++ src/parse.c
@@ -6,10 +6,31 @@
 /*   By: alago-ga <alago-ga@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:00:37 by alago-ga          #+#    #+#             */
-/*   Updated: 2026/02/23 17:01:39 by alago-ga         ###   ########.fr       */
+/*   Updated: 2026/02/23 18:00:09 by alago-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+char	*get_color(char map_name, char c)
+{
+	int	fd;
 
+	fd = open(file);
+}
+
+int	parse(t_map *map, char *map_name)
+{
+	map = ft_calloc(1, sizeof t_map);
+	if (!map)
+		return (write(2, "malloc failed\n", 14), FAILURE);
+	map.width = get_map_width(map_name);
+	map.length = get_map_length(map_name);
+	map.player_x = get_x_pos(map_name);
+	map.player_y = get_y_pos(map_name);
+	map.player_dir = get_player_dir(map_name);
+	map.walls = get_wall_paths(map_name);
+	map.floor_color = get_color(map_name, f);
+	map.ceiling_color = get_color(map_name, c);
+	return (0);
+}
