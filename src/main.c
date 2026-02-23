e3a986891e177e7b988ea439a2f0425956c72f3d
Ariel Lago Garcia <alago-ga@c4c3c2.42berlin.de>
2026-02-23 17:30:34 +0100

    add simple main that calls parse

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alago-ga <alago-ga@student.42berlin.d>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/23 17:22:57 by alago-ga          #+#    #+#             */
+/*   Updated: 2026/02/23 17:29:53 by alago-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	main(int argc, char *argv[])
+{
+	t_game	*game;
+	t_map	*map;
+
+	if (argc != 2)
+		return (write(2, "Try adding a map\n", 17), 1);
+	if (parse(&map, argv[1]) == FAILURE)
+		return (1);
+	return (0);
+}
