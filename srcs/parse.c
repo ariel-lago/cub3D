1b02c204d2532f50139bc9f2adf8d9f8a8515f90
Ariel Lago Garcia <alago-ga@c3a4c2.42berlin.de>
2026-02-25 16:27:03 +0100

    new parsing method

--- srcs/parse.c
+++ srcs/parse.c
@@ -14,49 +14,24 @@
 
 //looks for the identifier and returns the path/color values attached
 //note: should I add an is space function or do we accept the subjet means a normal ' ' space?
-static char	*get_info(int fd, char *id)
+static char	*get_info(char *line, int *identifiers)
 {
 	int		i;
-	char	*line;
 	char	*ret;
 
-	line = get_next_line(fd);
-	while (line)
+	i = 0;
+	while (line[i] == ' ')
+		i++;
+	ret = ft_strdup(&line[i]);
+	if (ret && (ret[ft_strlen(ret) - 1] == '\n'))
 	{
-		i = 0;
-		while (line[i] == ' ')
-			i++;
-		if (ft_strncmp(&line[i], id, ft_strlen(id)) == SUCCESS)
-		{
-			i += ft_strlen(id);
-			while (line[i] == ' ')
-				i++;
-			ret = ft_strdup(&line[i]);
-			if (ret && (ret[ft_strlen(ret) - 1] == '\n'))
-				ret[ft_strlen(ret) - 1] = '\0';
-			free(line);
-			return (ret);
-		}
-		free (line);
-		line = get_next_line(fd);
+		ret[ft_strlen(ret) - 1] = '\0';
+		*identifiers = *identifiers + 1;
+		return (ret);
 	}
-	return (error("Error\nMissing identifier"), NULL);
+	return (NULL);
 }
 
-static int	get_wall_paths(int fd, char *wall_paths[])
-{
-	wall_paths[0] = get_info(fd, "NO");
-	printf("NO: %s\n", wall_paths[0]);
-	wall_paths[1] = get_info(fd, "SO");
-	printf("SO: %s\n", wall_paths[1]);
-	wall_paths[2] = get_info(fd, "WE");
-	printf("WE: %s\n", wall_paths[2]);
-	wall_paths[3] = get_info(fd, "EA");
-	printf("EA: %s\n", wall_paths[3]);
-	if (!wall_paths[0] || !wall_paths[1] || !wall_paths[2] || !wall_paths[3])
-		return (FAILURE);
-	return (SUCCESS);
-}
 /**
  * Frees a NULL-terminated array of strings created by ft_split
  * 
@@ -124,6 +99,37 @@ static int parse_rgb(char *rgb_str)
     return (color);
 }
 
+static int	find_identifiers(int fd, t_map *map)
+{
+	char	*line;
+	int		identifiers;
+
+	identifiers = 0;
+	line = get_next_line(fd);
+	while (line && identifiers < 6)
+	{
+		if (ft_strncmp(line,"NO ", 3) == 0 && !map->walls[0])
+			map->walls[0] = get_info(&line[3], &identifiers);
+		else if (ft_strncmp(line,"SO ", 3) == 0 && !map->walls[1])
+			map->walls[1] = get_info(&line[3], &identifiers);
+		else if (ft_strncmp(line,"WE ", 3) == 0 && !map->walls[2])
+			map->walls[2] = get_info(&line[3], &identifiers);
+		else if (ft_strncmp(line,"EA ", 3) == 0 && !map->walls[3])
+			map->walls[3] = get_info(&line[3], &identifiers);
+		else if (ft_strncmp(line,"F ", 2) == 0 && !map->floor_color)
+			map->floor_color = parse_rgb(get_info(&line[2], &identifiers));
+		else if (ft_strncmp(line,"C ", 2) == 0 && !map->ceiling_color)
+			map->ceiling_color = parse_rgb(get_info(&line[2], &identifiers));
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	if (identifiers != 6)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+
 int	parse(t_map *map, char *map_name)
 {
 	int	fd;
@@ -132,23 +138,18 @@ int	parse(t_map *map, char *map_name)
 	if (fd < 0)
 		return (FAILURE);
 	ft_memset(map, 0, sizeof (t_map));
-//	map.width = get_map_width(map_name);
-//	map.length = get_map_length(map_name);
 //	map.player_x = get_x_pos(map_name);
 //	map.player_y = get_y_pos(map_name);
 //	map.player_dir = get_player_dir(map_name);
-	if (get_wall_paths(fd, map->walls) == FAILURE)
-		return (close(fd), error("Error"), FAILURE);
-	map->floor_color = parse_rgb(get_info(fd, "F"));
-	printf("floor color: %d\n", map->floor_color);
-	map->ceiling_color = parse_rgb(get_info(fd, "C"));
-	printf("ceiling color: %d\n", map->ceiling_color);
-	if (!map->floor_color || !map->ceiling_color)
+	if (find_identifiers(fd, map) == FAILURE)
 		return (close(fd), FAILURE);
+	printf("floor color: %d\n", map->floor_color);
+	printf("ceiling color: %d\n", map->ceiling_color);
 	if (get_map_size(map_name, &map->map_height, &map->map_width) == FAILURE)
 		return (close(fd), FAILURE);
 	if (load_map(map_name, map) == FAILURE)
 		return (error("Error\nCould not load map"), FAILURE);
+	close(fd);
 	return (SUCCESS);
 }
 
