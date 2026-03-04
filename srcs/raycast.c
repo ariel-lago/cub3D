017b18185ee05ff70a627d1177ea062e1935c220
Ariel Lago Garcia <alago-ga@c4b10c2.42berlin.de>
2026-03-04 21:07:38 +0100

    add step

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -6,7 +6,7 @@
 /*   By: alago-ga <alago-ga@student.42berlin.d>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 19:50:57 by alago-ga          #+#    #+#             */
-/*   Updated: 2026/03/04 20:49:06 by alago-ga         ###   ########.fr       */
+/*   Updated: 2026/03/04 21:07:22 by alago-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ void	cast_rays(t_game *game)
 	x = 0;
 	while (x < WIN_WIDTH)
 	{
-		set_ray_dir(game, &game->ray, x)
+		set_ray_dir(game, &game->ray, x);
+		init_dda(game, &game->ray);
 		x++;
 	}
 }
@@ -43,19 +44,32 @@ void	set_ray_dir(t_game *game, t_ray *ray, int x)
 Calculates the distance the ray needs to travel to cross a square in x and y.
 Sets the map tile where the player is;
 Calculates distance from first square to the next crossing (since player is in center of square)
+sets stepx and y so we know whether it has to move in one or another direction in the axis (if raydir is positive it will be +1 andif negative -1)
 */
 void	init_dda(t_game *game, t_ray *ray)
 {
 	ray->dist.x = ;
 	ray->dist.y = ;
-	ray->map_x = game->player.pos.x;
-	ray->map_y = game->player.pos.y;
+	ray->map_x = (int)game->player.pos.x;
+	ray->map_y = (int)game->player.pos.y;
 	if (ray->raydir.x < 0)
+	{
+		ray->step_x = -1;
 		ray->first_dist.x = ;
+	}
 	else
+	{
 		ray->first_dist.x = ;
+		ray->step_x = 1;
+	}
 	if (ray->raydir.y < 0)
+	{
+		ray->step_y = -1;
 		ray->first_dist.y = ;
+	}
 	else
+	{
+		ray->step_y = 1;
 		ray->first_dist.y = ;
+	}
 }
