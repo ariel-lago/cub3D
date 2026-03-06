0127c275e0cb1e8872f956bfc38d50cfb4b6e70f
Ariel Lago Garcia <alago-ga@c3a8c4.42berlin.de>
2026-03-06 14:33:22 +0100

    dda loop

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -12,6 +12,104 @@
 
 #include "cub3d.h"
 
+/*typedef struct s_ray
+{
+	double		cam;
+	int			map_x;
+	int			map_y;
+	t_vector	dist;
+	t_vector	first_dist;
+	int			step_x;
+	int			step_y;
+	t_vector	raydir;
+	double		raypos;
+	int			wall_type;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+}	t_ray;
+
+/*
+ We set each column to a position in the camera plane (-1.0 to +1.0)
+ cam = 0 means it looks straight where the player looks
+ cam = 1 means ray leans all the way to the right edge
+ cam = -1 means ray leans to the left fully
+* */
+static void	set_ray_dir(t_game *game, t_ray *ray, int x)
+{
+	ray->cam = 2 * x / (double)WIN_WIDTH - 1;
+	ray->raydir.x = game->player.dir.x + game->player.plane.x * ray->cam;
+	ray->raydir.y = game->player.dir.y + game->player.plane.y * ray->cam;
+}
+
+/*
+Calculates the distance the ray needs to travel to cross a square in x and y.
+Sets the map tile where the player is(the pos but as an int so we just have the tile, not the pos in it);
+Calculates distance from first square to the next crossing (since player is in center of square)
+sets stepx and y so we know whether it has to move in one or another direction in the axis (if raydir is positive it will be +1 andif negative -1)
+Calculates the fist distance from the player position to the end of the tile:
+for this we see how far the player is from the tile wall and multiply it by the full distance.
+*/
+static void	init_dda(t_game *game, t_ray *ray)
+{
+	ray->dist.x = fabs(1.0/ray->raydir.x);
+	ray->dist.y = fabs(1.0/ray->raydir.y);
+	ray->map_x = (int)game->player.pos.x;
+	ray->map_y = (int)game->player.pos.y;
+	if (ray->raydir.x < 0)
+	{
+		ray->step_x = -1;
+		ray->first_dist.x = (game->player.pos.x - ray->map_x) * ray->dist.x;
+	}
+	else
+	{
+		ray->first_dist.x = (ray->map_x + 1.0 - game->player.pos.x) * ray->dist.x;
+		ray->step_x = 1;
+	}
+	if (ray->raydir.y < 0)
+	{
+		ray->step_y = -1;
+		ray->first_dist.y = (game->player.pos.y - ray->map_y) * ray->dist.y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->first_dist.y = (ray->map_y + 1.0 - game->player.pos.y) * ray->dist.y;
+	}
+	dda_loop(game, ray);
+}
+
+/*
+Checks whether first_dist .x or .y is smaller
+Moves to closest one in the direction of step
+adds dist.x or dist.y to first_dist .x .y
+sets the wall type to 0 for vertical and 1 for horizontal
+checks if its a wall (stops if it is or continues otherwise)
+*/
+static void	dda_loop(t_game *game, t_ray *ray)
+{
+	while (1)
+	{
+		if (ray->first_dist.x < ray->first_dist.y)
+		{
+			ray->map.x += ray->step_x;
+			ray->first_dist.x += ray->dist.x;
+			ray->wall_type = 0;
+		}
+		else
+		{
+			ray->map.y += ray->step_y;
+			ray->first_dist.y += ray->dist.y;
+			ray->wall_type = 1;
+		}
+		if (game->map.map[ray->map_y][ray->map_x] == '1')
+		{
+			get_wall_size(ray);
+			break ;	
+		}
+	}
+}
+
 /*
 for every column of win width we cast a ray till the next wall
 */
@@ -25,51 +123,5 @@ void	cast_rays(t_game *game)
 		set_ray_dir(game, &game->ray, x);
 		init_dda(game, &game->ray);
 		x++;
-	}
-}
-/*
- We set each column to a position in the camera plane (-1.0 to +1.0)
- cam = 0 means it looks straight where the player looks
- cam = 1 means ray leans all the way to the right edge
- cam = -1 means ray leans to the left fully
-* */
-void	set_ray_dir(t_game *game, t_ray *ray, int x)
-{
-	ray->cam = 2 * x / (double)WIN_WIDTH - 1;
-	ray->raydir.x = game->player.dir.x + game->player.plane.x * ray->cam;
-	ray->raydir.y = game->player.dir.y + game->player.plane.y * ray->cam;
-}
-
-/*
-Calculates the distance the ray needs to travel to cross a square in x and y.
-Sets the map tile where the player is;
-Calculates distance from first square to the next crossing (since player is in center of square)
-sets stepx and y so we know whether it has to move in one or another direction in the axis (if raydir is positive it will be +1 andif negative -1)
-*/
-void	init_dda(t_game *game, t_ray *ray)
-{
-	ray->dist.x = ;
-	ray->dist.y = ;
-	ray->map_x = (int)game->player.pos.x;
-	ray->map_y = (int)game->player.pos.y;
-	if (ray->raydir.x < 0)
-	{
-		ray->step_x = -1;
-		ray->first_dist.x = ;
-	}
-	else
-	{
-		ray->first_dist.x = ;
-		ray->step_x = 1;
-	}
-	if (ray->raydir.y < 0)
-	{
-		ray->step_y = -1;
-		ray->first_dist.y = ;
-	}
-	else
-	{
-		ray->step_y = 1;
-		ray->first_dist.y = ;
 	}
 }
